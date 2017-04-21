a25c622ef7d95c8659283d2f59210aa956f2f413
bheinzelman <heinzelmanb@gmail.com>
2017-04-20 21:19:17 -0700

    added mobile touch events

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 	var ctx = canvas.getContext('2d');
 	var mouseDown = false;
 
+	const winWidth = $(document).width();
+	const winHeight = $(document).height();
+
+	var size = winHeight < winWidth ? winHeight : winWidth;
+
+	canvas.width = parseInt(size * .6);
+	canvas.height = parseInt(size * .6);
+
 	// we want the canvas to be 100 boxes wide, 100 boxes tall.
 	const canvasWidth = $(canvas).width();
 	const canvasHeight = $(canvas).height();
@@ -10,10 +18,16 @@ $(document).ready(function() {
 	const pixelWidth = canvasWidth/32;
 	const pixelHeight = canvasHeight/32;
 
+	//
+	// Clears the canvas.
+	//
 	function clear() {
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 	}
 
+	//
+	// On a touch event, draw a pixel to the corresponding
+	// box.
 	function draw(event) {
 		if (mouseDown) {
 			var rect = canvas.getBoundingClientRect(), 
@@ -33,6 +47,9 @@ $(document).ready(function() {
 		}
 	}
 
+	//
+	// Mouse event handlers
+	//
 	$("#canvas").mousemove(function(event) {
 		draw(event);
 	});
@@ -46,14 +63,47 @@ $(document).ready(function() {
 		mouseDown = false;		
 	});
 
-	$('#clear').click(clear);
+	//
+	// Mobile touch events
+	//
+	/* Drag */
+	$(canvas).on("touchmove", function (e) {
+  		var touch = e.touches[0];
+  		var mouseEvent = new MouseEvent("mousemove", {
+    		clientX: touch.clientX,
+    		clientY: touch.clientY
+  		});
+  		canvas.dispatchEvent(mouseEvent);
+	});
 
+	/* init touch */
+	$(canvas).on("touchstart", function (e) {
+  		var touch = e.touches[0];
+  		var mouseEvent = new MouseEvent("mousedown", {
+    		clientX: touch.clientX,
+    		clientY: touch.clientY
+  		});
+  		canvas.dispatchEvent(mouseEvent);
+	});
+
+	/* touch end */
+	$(canvas).on("touchend", function (e) {
+	  var mouseEvent = new MouseEvent("mouseup", {});
+	  canvas.dispatchEvent(mouseEvent);
+	});
+
+
+	//
+	// Button handlers
+	//
+	$('#clear').click(clear);
 	$("#erase").click(function() {
+		// set draw color to white
 		ctx.fillStyle = "#ffffff";
 		ctx.strokeStyle = "#ffffff";
 	});
-
 	$("#draw").click(function() {
+		// set draw color to black
 		ctx.fillStyle = "#000000";
 		ctx.strokeStyle = "#000000";	
 	});
