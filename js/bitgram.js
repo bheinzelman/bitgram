78af7e25a03d08ec7e38e3ceb474ff89d8858046
bheinzelman <heinzelmanb@gmail.com>
2017-04-20 22:14:14 -0700

    refactored to its own file

---
+++ js/bitgram.js
@@ -0,0 +1,135 @@
+function bitgram() {
+	this.canvas = null;
+	this.ctx = null;
+	this.mouseDown = false;
+	this.canvasWidth = null;
+	this.canvasHeight = null;
+	this.pixelWidth = null;
+	this.pixelHeight = null;
+
+	this.pixels = [];
+	for (var i = 0; i < 32; i++) {
+		var row = [];
+		for (var j = 0; j < 32; j++) {
+			row.push("#ffffff");
+		}
+		this.pixels.push(row);
+	}
+}
+
+bitgram.prototype.init = function() {
+	this.canvas = document.getElementById("canvas");
+	this.ctx = this.canvas.getContext('2d');
+	this.sizeCanvas();
+
+	//
+	// Mouse event handlers
+	//
+	$(this.canvas).mousemove((event) => {
+		this.draw(event);
+	});
+
+	$(this.canvas).mousedown((event) => {
+		this.mouseDown = true;		
+		this.draw(event);
+	});
+
+	$(this.canvas).mouseup(() => {
+		this.mouseDown = false;		
+	});
+
+	//
+	// Mobile touch events
+	//
+	/* Drag */
+	$(this.canvas).on("touchmove", (e) => {
+  		var touch = e.touches[0];
+  		var mouseEvent = new MouseEvent("mousemove", {
+    		clientX: touch.clientX,
+    		clientY: touch.clientY
+  		});
+  		this.canvas.dispatchEvent(mouseEvent);
+	});
+
+	/* init touch */
+	$(this.canvas).on("touchstart", (e) => {
+  		var touch = e.touches[0];
+  		var mouseEvent = new MouseEvent("mousedown", {
+    		clientX: touch.clientX,
+    		clientY: touch.clientY
+  		});
+  		this.canvas.dispatchEvent(mouseEvent);
+	});
+
+	/* touch end */
+	$(this.canvas).on("touchend", (e) => {
+	  var mouseEvent = new MouseEvent("mouseup", {});
+	  this.canvas.dispatchEvent(mouseEvent);
+	});
+
+	// resize the canvas 
+	$(window).resize(() => {
+		this.sizeCanvas();
+		for (var i = 0; i < 32; i++) {
+			for (var j = 0; j < 32; j++) {
+				this.setColor(this.pixels[i][j]);
+				this.drawPixel(i, j);
+			}
+		}
+	})
+}
+
+bitgram.prototype.sizeCanvas = function() {
+	const winWidth = $(document).width();
+	const winHeight = $(document).height();
+	const size = winHeight < winWidth ? winHeight : winWidth;
+	this.canvas.width = parseInt(size * .6);
+	this.canvas.height = parseInt(size * .6);
+
+	this.canvasWidth = $(this.canvas).width();
+	this.canvasHeight = $(this.canvas).height();
+
+	this.pixelWidth = this.canvasWidth/32;
+	this.pixelHeight = this.canvasHeight/32;
+}
+
+
+bitgram.prototype.clear = function() {
+	this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+}
+
+bitgram.prototype.draw = function(event) {
+	if (this.mouseDown) {
+		var rect = this.canvas.getBoundingClientRect(), 
+			scaleX = this.canvas.width / rect.width,    
+			scaleY = this.canvas.height / rect.height;  
+			
+		var x = ((event.clientX - rect.left) * scaleX);
+		var y = ((event.clientY - rect.top) * scaleY);
+
+		//get the cell indices
+		x = parseInt(x/this.pixelWidth);
+		y = parseInt(y/this.pixelHeight);
+
+		this.drawPixel(x, y);
+	}
+}
+
+/**
+ * Takes x coord and y coord
+ */
+bitgram.prototype.drawPixel = function(xCoord, yCoord) {
+	var x = xCoord * this.pixelWidth;
+	var y = yCoord * this.pixelHeight;
+
+	this.ctx.beginPath();
+	this.ctx.fillRect(x, y, this.pixelWidth, this.pixelHeight);
+	this.ctx.stroke();
+
+	this.pixels[xCoord][yCoord] = this.ctx.fillStyle;
+}
+
+bitgram.prototype.setColor = function(color) {
+	this.ctx.fillStyle = color;
+	this.ctx.strokeStyle = color;
+}
--- js/index.js
+++ js/index.js
@@ -1,110 +1,18 @@
 $(document).ready(function() {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext('2d');
-	var mouseDown = false;
-
-	const winWidth = $(document).width();
-	const winHeight = $(document).height();
-
-	var size = winHeight < winWidth ? winHeight : winWidth;
-
-	canvas.width = parseInt(size * .6);
-	canvas.height = parseInt(size * .6);
-
-	// we want the canvas to be 100 boxes wide, 100 boxes tall.
-	const canvasWidth = $(canvas).width();
-	const canvasHeight = $(canvas).height();
-
-	const pixelWidth = canvasWidth/32;
-	const pixelHeight = canvasHeight/32;
-
-	//
-	// Clears the canvas.
-	//
-	function clear() {
-		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-	}
-
-	//
-	// On a touch event, draw a pixel to the corresponding
-	// box.
-	function draw(event) {
-		if (mouseDown) {
-			var rect = canvas.getBoundingClientRect(), 
-				scaleX = canvas.width / rect.width,    
-				scaleY = canvas.height / rect.height;  
-				
-			var x = ((event.clientX - rect.left) * scaleX);
-			var y = ((event.clientY - rect.top) * scaleY);
-
-			x = parseInt(x/pixelWidth) * pixelWidth;
-			y = parseInt(y/pixelHeight) * pixelHeight;
-
-
-			ctx.beginPath();
-			ctx.fillRect(x, y, pixelWidth, pixelHeight);
-			ctx.stroke();
-		}
-	}
-
-	//
-	// Mouse event handlers
-	//
-	$("#canvas").mousemove(function(event) {
-		draw(event);
-	});
-
-	$("#canvas").mousedown(function(event) {
-		mouseDown = true;		
-		draw(event);
-	});
-
-	$("#canvas").mouseup(function() {
-		mouseDown = false;		
-	});
-
-	//
-	// Mobile touch events
-	//
-	/* Drag */
-	$(canvas).on("touchmove", function (e) {
-  		var touch = e.touches[0];
-  		var mouseEvent = new MouseEvent("mousemove", {
-    		clientX: touch.clientX,
-    		clientY: touch.clientY
-  		});
-  		canvas.dispatchEvent(mouseEvent);
-	});
-
-	/* init touch */
-	$(canvas).on("touchstart", function (e) {
-  		var touch = e.touches[0];
-  		var mouseEvent = new MouseEvent("mousedown", {
-    		clientX: touch.clientX,
-    		clientY: touch.clientY
-  		});
-  		canvas.dispatchEvent(mouseEvent);
-	});
-
-	/* touch end */
-	$(canvas).on("touchend", function (e) {
-	  var mouseEvent = new MouseEvent("mouseup", {});
-	  canvas.dispatchEvent(mouseEvent);
-	});
-
-
+	var bg = new bitgram();
+	bg.init();
+	
 	//
 	// Button handlers
 	//
-	$('#clear').click(clear);
+	$('#clear').click(function() {
+		bg.clear();
+	});
 	$("#erase").click(function() {
 		// set draw color to white
-		ctx.fillStyle = "#ffffff";
-		ctx.strokeStyle = "#ffffff";
+		bg.setColor("#ffffff");
 	});
 	$("#draw").click(function() {
-		// set draw color to black
-		ctx.fillStyle = "#000000";
-		ctx.strokeStyle = "#000000";	
-	});
+		bg.setColor("#000000");
+	});	
 });
